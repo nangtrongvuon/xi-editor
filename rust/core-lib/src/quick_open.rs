296e4348ee5b27f9476b8ce7bbccc9256555de73
Dung Le <dungleviethoang@gmail.com>
2019-02-22 15:57:38 +0700

    Switched to new linear fuzzy find algorithm

--- rust/core-lib/src/quick_open.rs
+++ rust/core-lib/src/quick_open.rs
@@ -1,6 +1,7 @@
 extern crate walkdir;
 
 use std::path::{Path, PathBuf};
+use std::cmp::{max};
 use std::collections::{BTreeMap};
 use walkdir::{DirEntry, WalkDir};
 
@@ -11,6 +12,14 @@ use walkdir::{DirEntry, WalkDir};
 // Suggestions are scored similarly to Sublime's own quick open.
 // Based heavily on FTS's fuzzy find code.
 
+const SCORE_MATCH: usize = 16;
+const SCORE_GAP_START: usize = 3;
+const SCORE_GAP_EXTENSION: usize = 1;
+
+const BONUS_BOUNDARY: usize = SCORE_MATCH / 2;
+const BONUS_CAMEL: usize = BONUS_BOUNDARY + SCORE_GAP_EXTENSION;
+const BONUS_CONSECUTIVE: usize = SCORE_GAP_START + SCORE_GAP_EXTENSION;
+const BONUS_FIRST_CHAR_MULTIPLIER: usize = 2;
 
 pub struct QuickOpen {
 	workspace_items: Vec<PathBuf>
@@ -28,7 +37,7 @@ impl QuickOpen {
 		fn is_not_hidden(entry: &DirEntry) -> bool {
 			entry.file_name()
 				 .to_str()
-				 .map(|s| entry.depth() == 0 || !s.starts_with("."))
+				 .map(|s| entry.depth() == 0 || !s.starts_with('.'))
 				 .unwrap_or(false)
 		}
 
@@ -46,151 +55,128 @@ impl QuickOpen {
 		eprintln!("{:?}", self.workspace_items);
 	}
 
-	// Returns true if every char in pattern is found in string
-	fn fuzzy_match_simple(pattern: &str, string: &str) -> bool {
+	// Returns true if every char in pattern is found in text
+	fn fuzzy_match_simple(pattern: &str, text: &str) -> bool {
 		let mut count = 0;
 		let mut pattern_chars = pattern.chars();
 
-		for chr in string.chars() {
+		for chr in text.chars() {
 			if let Some(str_char) = pattern_chars.next() {
 				if chr.to_lowercase().next() == str_char.to_lowercase().next() {
 					count += 1;
 				}
 			}
 		}
-
-		return count == pattern.len()
+		
+		count == pattern.len()
 	}
 
-	fn fuzzy_match(pattern: &str, string: &str) -> (bool, usize) {
-		fn fuzzy_match_recursive(pattern: &str, string: &str, mut score: usize, original_string: &str, mut source_matches: Vec<usize>, mut matches: Vec<usize>, max_matches: usize, next_match: usize, mut recursion_count: usize, recursion_limit: usize) -> (bool, usize) {
-			recursion_count += 1;
-			if recursion_count >= recursion_limit {
-				return (false, score)
-			}
-
-			let mut pattern_idx = 0;
-			let mut recursive_match = false;
-			let mut best_recursive_matches: Vec<usize> = Vec::new();
-			let best_recursive_score = 0;
-
-			let string_chars = string.chars();
-			let mut pattern_chars = pattern.chars().peekable();
-
-			let mut first_match = true;
-
-			for chr in string_chars {
-				// println!("{}", chr);
-				if let Some(pattern_char) = pattern_chars.peek() {
-					// println!("pattern: {}", pattern_char);
-					if chr.to_lowercase().next() == pattern_char.to_lowercase().next() {
-						// println!("match: {}", pattern_char);
-						if next_match >= max_matches {
-							return (false, score)
-						}
-
-						let mut recursive_matches: Vec<usize> = Vec::new();
-						let recursive_score = 0;
-
-						if first_match {
-							matches.append(&mut source_matches);
-							first_match = false
-						}
-
-						let (next_fuzzy, _) = fuzzy_match_recursive(pattern, &string[1..], recursive_score, original_string, matches.to_owned(), recursive_matches.to_owned(), max_matches, next_match, recursion_count, recursion_limit);
-
-						if next_fuzzy {
-							if !recursive_match || recursive_score > best_recursive_score {
-								best_recursive_matches.append(&mut recursive_matches)
-							}
-
-							recursive_match = true;
-						}
-
-						pattern_idx += 1;
-						pattern_chars.next();
-						if string.len() > original_string.len() {
-							matches.push(string.len() - original_string.len());
-						} else {
-							matches.push(original_string.len() - string.len());
-						}
-					}
-				}
-			}
-
-			let matched = if pattern_idx == pattern.len() {
-				true
-			} else {
-				false
-			};
-
-			if matched {
-				let adjacent_bonus = 15;
-				let separator_bonus = 30;
-				let camel_bonus = 30;
-				let first_letter_bonus = 15;
-
-				let leading_letter_penalty = 5;
-				let max_leading_letter_penalty = 15;
-				let unmatched_letter_penalty = 1;
-
-				score = 100;
-
-				let mut penalty = leading_letter_penalty * matches[0];
-				if penalty < max_leading_letter_penalty {
-					penalty = max_leading_letter_penalty;
-				}
-				score -= penalty;
-
-				let unmatched = string.len() - original_string.len() - next_match;
-				score -= unmatched * unmatched_letter_penalty;
-
-				for i in 0..next_match {
-					let prev_idx;
-					let current_idx = matches[i];
-					if i > 0 {
-						prev_idx = matches[i - 1];
-
-						if current_idx == prev_idx + 1 {
-							score += adjacent_bonus;
-						}
-					}
-
-					if current_idx > 0 {
-						let original_chars: Vec<char> = original_string.chars().collect();
-						let neighbour = original_chars[current_idx - 1];
-						let current = original_chars[current_idx];
-
-						if neighbour.is_lowercase() && current.is_uppercase() {
-							score += camel_bonus;
-						}
-
-						if neighbour == '_' || neighbour == '-' {
-							score += separator_bonus;
-						}
-
-					} else {
-						score += first_letter_bonus;
-					}
-				}
-
-				if recursive_match && (!matched || best_recursive_score > score) {
-					matches.append(&mut best_recursive_matches);
-					score = best_recursive_score;
-					return (true, score)
-				} else if matched {	
-					return (true, score)
-				} else {
-					return (false, 0)
-				}
-			}
-
-			return (matched, score)
+	fn fuzzy_match(&self, pattern: &str, text: &str) -> (bool, usize) {
+		if pattern.len() == 0 {
+			return (false, 0)
 		}
 
-		let recursion_count = 0;
-		let recursion_limit = 10;
+		let mut p_index = 0;
+		let mut start_index = 0;
+		let mut end_index = 0;
 
-		return fuzzy_match_recursive(pattern, string, 0, pattern, Vec::new(), Vec::new(), 50, 0, recursion_count, recursion_limit)
+		let pattern_length = pattern.len();
+		let text_length = text.len();
+
+		for i in 0..text_length {
+			let char_index = text_length - i - 1;
+			let pattern_index = pattern_length - p_index - 1;
+
+			if let (Some(current_char), Some(pattern_char)) = (text.chars().nth(char_index), pattern.chars().nth(pattern_index)) {
+				if current_char == pattern_char {
+					if start_index == 0 {
+						start_index = i
+					}
+
+					p_index = p_index + 1;
+
+					if p_index == pattern_length {
+						end_index = i + 1;
+						break
+					}
+				}
+			}
+		}
+
+		if start_index >= 0 && end_index >= 0 {
+
+			for i in (start_index..end_index - 1).rev() {
+				let mut second_text_index = text_length - i - 1;
+				let mut second_p_index = pattern_length - p_index - 1;	
+
+				if let (Some(current_char), Some(pattern_char)) = (text.chars().nth(second_text_index), pattern.chars().nth(second_p_index)) {
+					if current_char == pattern_char {
+						p_index = p_index - 1;
+						if p_index == 0 {
+							start_index = i;
+							break
+						}
+					}
+				}
+			}
+
+			let score = self.calculate_score(pattern, text, start_index, end_index);
+			return (true, score)
+		}
+
+		start_index = text_length - end_index;
+		end_index = text_length - start_index;
+
+		return (false, 0)
+	}
+
+	fn calculate_score(&self, pattern: &str, text: &str, start_index: usize, end_index: usize) ->usize {
+
+		let mut pattern_index = 0;
+		let mut score = 0;
+		let mut in_gap = false;
+		let mut consecutive = 0;
+		let mut first_bonus = 0;
+
+		for i in start_index..end_index {
+			if let (Some(text_char), Some(pattern_char)) = (text.chars().nth(i), pattern.chars().nth(pattern_index)) {
+				if text_char == pattern_char {
+					score = score + SCORE_MATCH;
+					let mut bonus = {
+						0
+					};
+					if consecutive == 0 {
+						first_bonus = bonus;
+					} else {
+						if bonus == BONUS_BOUNDARY {
+							first_bonus = bonus;
+						}
+						bonus = max(max(bonus, first_bonus), BONUS_CONSECUTIVE);
+					}
+
+					if pattern_index == 0 {
+						score = score + bonus * BONUS_FIRST_CHAR_MULTIPLIER;
+					} else {
+						score = score + bonus;
+					}
+
+					in_gap = false;
+					consecutive = consecutive + 1;
+					pattern_index = pattern_index + 1;
+				} else {
+					if in_gap {
+						score = score + SCORE_GAP_EXTENSION;
+					} else {
+						score = score + SCORE_GAP_START;
+					}
+
+					in_gap = true;
+					consecutive = 0;
+					first_bonus = 0;
+				}
+			}
+		}
+
+		return score
 	}
 }
